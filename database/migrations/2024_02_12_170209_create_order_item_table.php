298ce3f4655ef13a7fd6c0fc5df81a7d254a3253
Ann <annasem@mail.ru>
2024-02-12 20:19:28 +0300

    Migrations task

---
+++ database/migrations/2024_02_12_170209_create_order_item_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_item', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('order');
+            $table->foreignId('item_id')->constrained('items');
+            $table->unsignedBigInteger('quantity');
+            $table->unsignedBigInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_item');
+    }
+};
